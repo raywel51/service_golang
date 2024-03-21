ff10f4bb63900f7f612db4fc869fb74bc86548ff
nontawatk <kittayawat.aong@gmail.com>
2024-03-21 14:30:30 +0700

    add code for sent mqtt json

--- main.go
+++ main.go
@@ -1,38 +1,47 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"github.com/streadway/amqp"
 	"log"
 	"math/rand"
 	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/streadway/amqp"
 )
 
 func main() {
+	r := gin.Default()
+	r.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.GET("/plate/random", func(ctx *gin.Context) {
 
-	minValue := 2000
-	maxValue := 60000
+		licensePlate := GenerateThaiLicensePlate()
+		log.Println("ป้ายทะเบียน -> " + licensePlate)
 
-	go func() {
-		for {
-			licensePlate := GenerateThaiLicensePlate()
-
-			log.Println("ป้ายทะเบียน -> " + licensePlate)
-
-			err := ConnectToRabbitMQ(licensePlate)
-			if err != nil {
-				log.Printf("Error connecting to RabbitMQ: %v\n", err)
-			}
-
-			randomDelay := rand.Intn(maxValue-minValue+1) + minValue
-			log.Printf("Next data in %ds", randomDelay/1000)
-			time.Sleep(time.Duration(randomDelay) * time.Millisecond)
-
-			log.Printf("==============================================\n\n")
+		err := ConnectToRabbitMQ(licensePlate)
+		if err != nil {
+			log.Printf("Error connecting to RabbitMQ: %v\n", err)
 		}
-	}()
 
-	select {}
+		ctx.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.GET("/mqtt", func(ctx *gin.Context) {
+		licensePlate := GenerateThaiLicensePlate()
+		log.Println("ป้ายทะเบียน -> " + licensePlate)
+
+		err := sentJsonMqtt(licensePlate)
+		if err != nil {
+			log.Printf("Error connecting to RabbitMQ: %v\n", err)
+		}
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
 }
 
 func ConnectToRabbitMQ(message string) error {
@@ -46,7 +55,7 @@ func ConnectToRabbitMQ(message string) error {
 	defer func(conn *amqp.Connection) {
 		err := conn.Close()
 		if err != nil {
-
+			return
 		}
 	}(conn)
 
@@ -57,7 +66,7 @@ func ConnectToRabbitMQ(message string) error {
 	defer func(ch *amqp.Channel) {
 		err := ch.Close()
 		if err != nil {
-
+			return
 		}
 	}(ch)
 
@@ -91,6 +100,72 @@ func ConnectToRabbitMQ(message string) error {
 	log.Printf("Sent Message to RabbitMQ: %s %s\n", message, time.Now().Format(time.RFC3339))
 
 	return nil
+}
+
+type Message struct {
+	Data   Data `json:"data"`
+	Status int  `json:"status"`
+}
+
+type Data struct {
+	LicensePlate string `json:"license_plate"`
+}
+
+func sentJsonMqtt(plate string) error {
+	brokerURL := "amqp://" + "testoffice" + ":" + "sm2O0itJGrwP2NBz" + "@" + "mqtt.letmein.asia/"
+	queueName := "testoffice1"
+	conn, err := amqp.Dial(brokerURL)
+	if err != nil {
+		return err
+	}
+	defer func(conn *amqp.Connection) {
+		err := conn.Close()
+		if err != nil {
+			return
+		}
+	}(conn)
+
+	ch, err := conn.Channel()
+	if err != nil {
+		return err
+	}
+	defer func(ch *amqp.Channel) {
+		err := ch.Close()
+		if err != nil {
+
+		}
+	}(ch)
+
+	message := Message{
+		Data: Data{
+			LicensePlate: plate,
+		},
+		Status: 1,
+	}
+
+	jsonMessage, err := json.Marshal(message)
+	if err != nil {
+		panic(err)
+	}
+
+	err = ch.Publish(
+		"",
+		queueName,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "application/json",
+			Body:        jsonMessage,
+		},
+	)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Connected to RabbitMQ\n")
+	log.Printf("Sent Message to RabbitMQ: %s %s\n", message.Data.LicensePlate, time.Now().Format(time.RFC3339))
+
+	return err
 }
 
 func GenerateThaiLicensePlate() string {
